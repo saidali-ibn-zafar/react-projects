1e0994179a0c9b6d681557073b5aa3cf0f96aa4b
saidali_ibn_zafar <120341849+saidali-ibn-zafar@users.noreply.github.com>
2024-01-28 10:02:24 +0100

    Add files via upload

---
+++ 02-tic-tac-toe-game/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import TicTactToe from "./Components/TicTacToe/TicTactToe.jsx";
+
+function App() {
+  return (
+    <div>
+      <TicTactToe />
+    </div>
+  );
+}
+
+export default App;
